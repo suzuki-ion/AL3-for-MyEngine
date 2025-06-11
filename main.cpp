2b5204dca49484636659429fa2ef4d199bb828fa
鈴木 色音 <k024g0058@g.neec.ac.jp>
2025-06-11 12:41:54 +0900

    AL3[確認課題]03_01.3Dレールシューティング

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "TitleScene.h"
 #include "GameScene.h"
 #include "Base/Input.h"
 #include "Base/Renderer.h"
@@ -16,86 +15,13 @@ auto sKashipanEngine = std::make_unique<Engine>("KashipanEngine", 1920, 1080, tr
 // レンダラーへのポインタ
 Renderer *sRenderer = sKashipanEngine->GetRenderer();
 
-// タイトルシーンのインスタンスを生成
-std::unique_ptr<TitleScene> sTitleScene;
 // ゲームシーンのインスタンスを生成
-std::unique_ptr<GameScene> sGameScene;
-
-// シーン
-enum class Scene {
-	kUnknown = 0,
-	kTitle,
-	kGame,
-};
-
-// 現在のシーン
-Scene scene = Scene::kUnknown;
-
-// シーン切り替え処理
-void ChangeScene() {
-	switch (scene) {
-	case Scene::kTitle:
-		if (sTitleScene->IsFinalized()) {
-			// シーン変更
-			scene = Scene::kGame;
-			// 旧シーンの解放
-			sTitleScene.reset();
-			// 新シーンの生成と初期化
-			sGameScene = std::make_unique<GameScene>();
-			sGameScene->Initialize(sKashipanEngine.get());
-		}
-		break;
-
-	case Scene::kGame:
-		if (sGameScene->IsFinished()) {
-			// シーン変更
-			scene = Scene::kTitle;
-			// 旧シーンの解放
-			sGameScene->Finalize();
-			sGameScene.reset();
-			// 新シーンの生成と初期化
-			sTitleScene = std::make_unique<TitleScene>();
-			sTitleScene->Initialize(sKashipanEngine.get());
-		}
-		break;
-	}
-}
-
-void UpdateScene() {
-	switch (scene) {
-	case Scene::kTitle:
-		sTitleScene->Update();
-		break;
-	case Scene::kGame:
-		sGameScene->Update();
-		break;
-	}
-}
-
-void DrawScene() {
-	sRenderer->PreDraw();
-	switch (scene) {
-	case Scene::kTitle:
-		sTitleScene->Draw();
-		break;
-	case Scene::kGame:
-		sGameScene->Draw();
-		break;
-	}
-	sRenderer->PostDraw();
-}
+std::unique_ptr<GameScene> sGameScene = std::make_unique<GameScene>(sKashipanEngine.get());
 
 } // namespace
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
-	// 最初のシーンの初期化
-	scene = Scene::kTitle;
-	// タイトルシーンのインスタンスを生成
-	sTitleScene = std::make_unique<TitleScene>();
-	// タイトルシーンの初期化
-	sTitleScene->Initialize(sKashipanEngine.get());
-
 	// ウィンドウのxボタンが押されるまでループ
 	while (sKashipanEngine->ProccessMessage() != -1) {
 		sKashipanEngine->BeginFrame();
@@ -104,17 +30,12 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		}
 		Input::Update();
 
-		// シーン切り替え処理
-		ChangeScene();
-
-		// シーン更新処理
-		UpdateScene();
-		// シーン描画処理
-		DrawScene();
+        // ゲームシーンの更新と描画
+        sGameScene->Update();
+        sGameScene->Draw();
 	}
 
     sGameScene.reset();
-    sTitleScene.reset();
 
 	return 0;
 }
