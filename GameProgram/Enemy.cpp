cb8044a89e4c40263fe363e8b2b9f9ac6630724c
鈴木 色音 <k024g0058@g.neec.ac.jp>
2025-06-23 18:30:33 +0900

    AL3[確認課題]03_08.敵の弾を一定間隔ごとに発射

--- GameProgram/Enemy.cpp
+++ GameProgram/Enemy.cpp
@@ -7,8 +7,19 @@
 using namespace KashipanEngine;
 
 namespace {
+
 // エンジンへのポインタ
 Engine *sKashipanEngine = nullptr;
+
+Vector3 TransformNormal(const Vector3 &v, const Matrix4x4 &m) {
+    // ベクトルを変換するための行列を適用
+    Vector3 result;
+    result.x = v.x * m.m[0][0] + v.y * m.m[1][0] + v.z * m.m[2][0];
+    result.y = v.x * m.m[0][1] + v.y * m.m[1][1] + v.z * m.m[2][1];
+    result.z = v.x * m.m[0][2] + v.y * m.m[1][2] + v.z * m.m[2][2];
+    return result;
+}
+
 }
 
 Enemy::Enemy(Engine *kashipanEngine) {
@@ -20,9 +31,12 @@ Enemy::Enemy(Engine *kashipanEngine) {
     model_ = std::make_unique<Model>("Resources/Enemy", "enemy.obj");
     model_->SetRenderer(renderer);
 
+    bulletModel_ = std::make_unique<Model>("Resources/Bullet", "bullet.obj");
+    bulletModel_->SetRenderer(renderer);
+
     // ワールド変換データの設定
     worldTransform_ = std::make_unique<WorldTransform>();
-    worldTransform_->translate_ = { -16.0f, -8.0f, 32.0f };
+    worldTransform_->translate_ = { -16.0f, -8.0f, 128.0f };
 
     // 初期フェーズの設定
     ChangeState(std::make_unique<EnemyStateApproach>(sKashipanEngine, this));
@@ -43,8 +57,48 @@ void Enemy::Update() {
     if (worldTransform_->translate_.z < 0.0f) {
         ChangeState(std::make_unique<EnemyStateLeave>(sKashipanEngine, this));
     }
+
+    // 弾の発射処理
+    Fire();
+    // 弾の更新
+    for (auto &bullet : bullets_) {
+        bullet->Update();
+    }
+    // 弾の削除処理
+    bullets_.remove_if([](const std::unique_ptr<EnemyBullet> &bullet) {
+        return !bullet->IsAlive();
+    });
 }
 
 void Enemy::Draw() {
     model_->Draw(*worldTransform_);
+    
+    // 弾を描画
+    for (auto &bullet : bullets_) {
+        bullet->Draw();
+    }
+}
+
+void Enemy::Fire() {
+    const Vector3 kBulletVelocity(0.0f, 0.0f, -6.0f);
+
+    // タイマーをカウント
+    fireTimer_ += sKashipanEngine->GetDeltaTime();
+    // タイマーが一定時間を超えたら弾を発射
+    if (fireTimer_ < kFireInterval) {
+        // 発射間隔に達していない場合は何もしない
+        return;
+    }
+
+    bullets_.push_back(
+        std::make_unique<EnemyBullet>(
+            sKashipanEngine,
+            bulletModel_.get(),
+            worldTransform_->translate_,
+            TransformNormal(kBulletVelocity, worldTransform_->worldMatrix_),
+            5.0f
+        )
+    );
+    // タイマーをリセット
+    fireTimer_ = 0.0f;
 }
--- GameProgram/Enemy.h
+++ GameProgram/Enemy.h
@@ -2,13 +2,16 @@
 #include <KashipanEngine.h>
 #include <Objects.h>
 #include <Objects/WorldTransform.h>
+#include <list>
 #include <memory>
 
 #include "BaseEnemyState.h"
+#include "EnemyBullet.h"
 
 class Enemy {
 public:
     static inline const float kMoveSpeed = 3.0f;
+    static inline const float kFireInterval = 1.0f;
 
     Enemy(Engine *kashipanEngine);
 
@@ -23,12 +26,22 @@ public:
     void Draw();
 
 private:
+    // 弾発射処理
+    void Fire();
+
     // ワールド変換データ
     std::unique_ptr<KashipanEngine::WorldTransform> worldTransform_;
     // モデル
     std::unique_ptr<KashipanEngine::Model> model_;
+    // 弾のモデル
+    std::unique_ptr<KashipanEngine::Model> bulletModel_;
 
     // 状態管理
     std::unique_ptr<BaseEnemyState> state_;
+
+    // 弾のリスト
+    std::list<std::unique_ptr<EnemyBullet>> bullets_;
+    // 弾の発射間隔タイマー
+    float fireTimer_ = 0.0f;
 };
 
