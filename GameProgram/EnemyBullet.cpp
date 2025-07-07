57e867a12bb754ebbe38b6709ba2813a39888dd4
鈴木 色音 <k024g0058@g.neec.ac.jp>
2025-07-07 12:48:05 +0900

    AL3[確認課題]03_09_ex1.弾を進行方向に向ける

--- GameProgram/EnemyBullet.cpp
+++ GameProgram/EnemyBullet.cpp
@@ -1,12 +1,14 @@
 #include "EnemyBullet.h"
 
+using namespace KashipanEngine;
+
 namespace {
 // エンジンへのポインタ
 Engine *sKashipanEngine = nullptr;
 }
 
-EnemyBullet::EnemyBullet(Engine *kashipanEngine, KashipanEngine::Model *model, const KashipanEngine::Vector3 &position,
-    const KashipanEngine::Vector3 &velocity, float lifeTime) : kLifeTime(lifeTime) {
+EnemyBullet::EnemyBullet(Engine *kashipanEngine, Model *model, const Vector3 &position,
+    const Vector3 &velocity, float lifeTime) : kLifeTime(lifeTime) {
     if (model == nullptr) {
         throw std::invalid_argument("Model pointer cannot be null");
     }
@@ -15,8 +17,15 @@ EnemyBullet::EnemyBullet(Engine *kashipanEngine, KashipanEngine::Model *model, c
     }
     sKashipanEngine = kashipanEngine;
     model_ = model;
-    worldTransform_ = std::make_unique<KashipanEngine::WorldTransform>();
+    worldTransform_ = std::make_unique<WorldTransform>();
     worldTransform_->translate_ = position;
+    worldTransform_->scale_ = Vector3(1.0f, 1.0f, 3.0f);
+    // 進行方向に向くように回転を設定
+    worldTransform_->rotate_ = {
+        atan2f(velocity.y, velocity.x),
+        atan2f(velocity.z, sqrtf(velocity.x * velocity.x + velocity.y * velocity.y)),
+        0.0f
+    };
     velocity_ = velocity;
     lifeTimeCounter_ = 0.0f;
     isAlive_ = true;
